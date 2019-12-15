518084d9190d8a9a455d9304007b0b33b47be45f
Catherine <wangrt@cn.ibm.com>
2019-12-15 22:26:23 +0800

    public

--- src/test/java/com/smc/SearchApplicationTests.java
+++ src/test/java/com/smc/SearchApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SearchApplicationTests {
+public class SearchApplicationTests {
 //
 //	@Autowired
 //	private StockPriceService stockPriceService;
