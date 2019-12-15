c68527a8b980870b35e799dfcfee1c4de1ecf385
Catherine <wangrt@cn.ibm.com>
2019-12-15 22:18:14 +0800

    test file modified

--- src/test/java/com/smc/SearchApplicationTests.java
+++ src/test/java/com/smc/SearchApplicationTests.java
@@ -4,23 +4,26 @@ import com.smc.service.StockPriceService;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SearchApplicationTests {
-
-	@Autowired
-	private StockPriceService stockPriceService;
-
-
-	@Test
-	public void testSingleCompanyChart() {
-		LocalDateTime start = LocalDateTime.parse((CharSequence) "2019-11-30 16:55:55",
-				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-		LocalDateTime end = LocalDateTime.parse((CharSequence) "2019-11-30 16:55:55",
-				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-		stockPriceService.comparisonSingleCompany("345902", start, end);
-	}
-
+//
+//	@Autowired
+//	private StockPriceService stockPriceService;
+//
+//
+//	@Test
+//	public void testSingleCompanyChart() {
+//		LocalDateTime start = LocalDateTime.parse((CharSequence) "2019-11-30 16:55:55",
+//				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//		LocalDateTime end = LocalDateTime.parse((CharSequence) "2019-11-30 16:55:55",
+//				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//		stockPriceService.comparisonSingleCompany("345902", start, end);
+//	}
+//
 }
